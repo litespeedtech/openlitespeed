51510304a30a6b5971d50d6c6234a7f8c9d3bc5c
BiZoNiX <bizonix@me.com>
2014-11-05 07:40:09 +0200

    change LSAPI to 6.7 version

--- dist/admin/misc/build_admin_php.sh
+++ dist/admin/misc/build_admin_php.sh
@@ -16,7 +16,7 @@ PHP_BUILD_DIR=$LSWS_HOME/phpbuild
 mkdir ${PHP_BUILD_DIR}
 LOG_FILE=${PHP_BUILD_DIR}/adminphp.log
 DL_METHOD="curl -L -o"
-LSAPI_VERSION=6.6
+LSAPI_VERSION=6.7
 PHP_CONF_OPTIONS="--prefix=/tmp --disable-all --with-litespeed --enable-session --enable-posix --enable-xml --with-libexpat-dir=/usr --with-zlib --enable-sockets --enable-bcmath --enable-json"
 
 PLF=`uname -p`
