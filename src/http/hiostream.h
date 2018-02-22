decf8048fc25fcf2162baf697ad1082730e30768
David <dxu@litespeedtech.com>
2018-02-22 14:44:52 -0500

    Fix two bug that cause websocket does not work

--- src/http/hiostream.h
+++ src/http/hiostream.h
@@ -291,7 +291,8 @@ public:
         if (pStream)
         {
             m_pStream = NULL;
-            pStream->setHandler(NULL);
+            if (pStream->getHandler() == this)
+                pStream->setHandler(NULL);
         }
         return pStream;
     }
